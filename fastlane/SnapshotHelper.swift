1d43b2a389c106db172a0cc773b81615fd6710f5
LucasMW <great-lucas@hotmail.com>
2020-05-14 03:00:49 +0100

    fastlane

--- fastlane/SnapshotHelper.swift
+++ fastlane/SnapshotHelper.swift
@@ -1,12 +1,5 @@
 //
 //  SnapshotHelper.swift
-//  MWN-SWIFT-UIUITests
-//
-//  Created by Lucas Menezes on 5/13/20.
-//  Copyright © 2020 Lucas Menezes. All rights reserved.
-//
-//
-//  SnapshotHelper.swift
 //  Example
 //
 //  Created by Felix Krause on 10/8/15.
